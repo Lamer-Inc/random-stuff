23a2e2bed3c07041349a4612bf59861cc147ffe4
Lamer-inc <66159424+Lamer-Inc@users.noreply.github.com>
2022-01-23 15:52:27 +0100

    Update anti-scam-bot.py

--- anti-scam-bot.py
+++ anti-scam-bot.py
@@ -7,6 +7,7 @@ token = 'OTMyMjA3MTQwMzUxOTMwMzkw.YePnjQ.D9M_VQC9t6c**********'
 print("Applicazione avviata")
 
 b = commands.Bot(command_prefix="!")
+b.remove_command("help")
 
 
 @b.command(name='report')
@@ -34,8 +35,8 @@ async def add_link(ctx, *, arg):
         await ctx.send('You cannot use this command, contact the bot owner')
 
 
-@b.command(name='scan')
-async def scan(ctx, *, arg):
+@b.command(name='scan_database')
+async def scan_database(ctx, *, arg):
     if arg in scam_link:
         await ctx.send('Link is in ours scam link database, please pay attention!')
     else:
@@ -73,6 +74,67 @@ async def check(ctx, *, arg):
     await ctx.send(embed=embed)
 
 
+@b.command(name='urlscan')
+async def urlscan(ctx, *, url):
+    headers = {'API-Key': '***********************', 'Content-Type': 'application/json'}
+    data = {"url": url, "visibility": "public"}
+    res = requests.post('https://urlscan.io/api/v1/scan/', headers=headers, data=json.dumps(data)).json()
+    risultato = res['result']
+    embed = discord.Embed(title='**Analisi del link richiesto: **', color=discord.Color.green())
+    embed.add_field(name='**Link scansionato** :', value=str(url), inline=False)
+    embed.add_field(name='**Link della scansione con urlscan.io**: ', value=risultato, inline=False)
+    embed.add_field(name='Crediti: ',
+                    value='Scansione effettuata su https://urlscan.io utilizzando le API messe a disposizione dal '
+                          'sito. Programma realizzato interamente da DiStRuTtOrE_Tm#6449 ( '
+                          'ID=586202654087184384)', inline=False)
+    await ctx.send(embed=embed)
+
+
+@b.command(name='help')
+async def help(ctx):
+    embed = discord.Embed(title='Aiuto e Crediti', color=discord.Color.red())
+    embed.add_field(name='**Lista Comandi**', value='Lista dei comandi del bot!', inline=False)
+    embed.add_field(name='!check [Es: !check youtube.com]', value='Esegue una scansione su '
+                                                                'https://www.ipqualityscore.com tramite le API '
+                                                                'reperibili qui: '
+                                                                'https://www.ipqualityscore.com/documentation'
+                                                                '/malicious-url-scanner-api/overview. Per utilizzare '
+                                                                'questo comando è necessario inserire il **dominio** '
+                                                                'del '
+                                                                'sito da scansionare. Tutti i risultati e lo '
+                                                                'strumento utilizzato '
+                                                                'appartengono ad https://www.ipqualityscore.com',
+                    inline=False)
+    embed.add_field(name='!urlscan [Es: !urlscan https://urlscan.io]', value="Esegue una scansione su "
+                                                                             "https://urlscan.io usando le API "
+                                                                             "reperibili qui: "
+                                                                             "https://urlscan.io/docs/api/. Per "
+                                                                             "utilizzare questo comando è necessario "
+                                                                             "inserire l'**url** del sito. Tutti i "
+                                                                             "risultati e lo strumento utilizzato "
+                                                                             "appartengono a https://urlscan.io",
+                    inline=False)
+    embed.add_field(name='**Crediti:**', value='API di https://www.ipqualityscore.com e https://urlscan.io', inline=False)
+    embed.add_field(name='GitHub con il codice del bot: ', value='https://github.com/Lamer-Inc/random-stuff/blob/main'
+                                                                 '/anti-scam-bot.py', inline=False)
+    embed.add_field(name='Creatore del bot: ', value='DiStRuTtOrE_Tm#6449 (ID=586202654087184384). Non replicare '
+                                                     'senza autorizzazione. Le API utilizzate appartengono ai '
+                                                     'rispettivi siti. Ogni uso è a puro fine informativo.',
+                    inline=False)
+    embed.add_field(name='Per finire...', value="Questo progetto nasce con l'idea di rendere Discord un po' più "
+                                                "sicuro. Ogni risultato è puramente informativo e le scansioni sono "
+                                                "effettuate dai rispettivi siti. DiStRuTtOrE_Tm#6449 non si assume "
+                                                "alcuna responsabilità per un utilizzo sbagliato del bot. Bot a solo "
+                                                "fine informativo, senza scopo di lucro.", inline=False)
+    embed.add_field(name='Differenza fra dominio e url: ', value='https://www.sistrix.it/chiedi-a-sistrix/ottimizzazione-onpage/qual-e-la-differenza-tra-url-dominio-sottodominio-nome-host-ecc/', inline=False)
+    await ctx.send(embed=embed)
+
+
 b.run(token)
 
 #NOT COMPLETED YET, DO NOT REPOST WITHOUT CONSENS
+
+#https://urlscan.io
+#https://www.ipqualityscore.com
+
+#ALL API USED ARE OF THE RESPECTIVE OWNERS. DO NOT USE WITHOUT CONSENS
